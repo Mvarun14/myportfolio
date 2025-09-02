15a8fab7c9e9009e3ca4eb3b576febbf006f6c15
MUNUKUNTLA PHANI VARUN <varungoud1402@gmail.com>
2025-09-02 11:39:38 -0400

    Changes

--- src/data/heroSectionData.ts
+++ src/data/heroSectionData.ts
@@ -14,7 +14,7 @@ export const heroSectionData = {
     },
     secondary: {
       label: 'Download Resume',
-      href: 'https://drive.google.com/drive/folders/13vvzvKlWokdxORM3xzbo7fSwEFNC96RP?usp=sharing',
+      href: '/MPhaniVarun.pdf',
       isExternal: true,
     },
   },
